cd185438e23d1706b31c843cfeb29b3e0ba0f568
Meatsushi64 <tzejenrex@gmail.com>
2025-01-22 23:39:21 +0800

    Renamed bot to Jen and changed greeting

---
+++ src/main/java/Jen.java
@@ -0,0 +1,10 @@
+public class Jen {
+    static String greeting = "Hello! I'm Jen. \n " +
+                      "What can I do for you?\n";
+    static String line = "_______________________\n";
+    static String goodbye = "Bye. Hope to see you again soon!\n";
+
+    public static void main(String[] args) {
+        System.out.println(line + greeting + line + goodbye + line);
+    }
+}
